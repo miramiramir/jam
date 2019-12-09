e3b359df662137f076ce404470340c83fa944aa1
ZaneSlick <Zane.png@protonmail.com>
2019-12-09 16:02:20 +0000

    Add update notifier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const { dumpAsciiLogo } = require('./util');
+
 const Web = require('./web');
 const Config = require('./config');
+const Updater = require('./Updater');
 const logger = require('./logger');
 const Process = require('./process');
 const path = require('path');
-const { dumpAsciiLogo } = require('./util');
 
 dumpAsciiLogo();
 
@@ -27,7 +29,8 @@ const { port } = Config.get('web');
 /**
  * Spawn
  */
-Web(port)
+Updater.checkForNewRelease()
+  .then(() => Web(port))
   .then(() => logger.info('Web Server Initialized...'))
   .then(() => process.spawn())
   .then(() => logger.info('Spawning Jam...'))
--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
+const notifier = require('node-notifier');
 
-const { version } = require('../../package.json');
 
 class Util {
   constructor() {
@@ -15,17 +15,26 @@ class Util {
     // eslint-disable-next-line no-console
     console.log(chalk.whiteBright([
       '',
-      '    -:://::-     ',
-      '    -//++++//-    ',
-      '   `:/++++++/:    ',
-      '  `::-            ',
-      '  .ooo+-    ./o`  ',
-      '   oooooo+/+ooo   ',
-      '   /oooooooooo/   ',
-      '   :oooooooooo-   ',
-      '    `-::::::.` ',
+      '             -:://::-     ',
+      '             -//++++//-    ',
+      '            `:/++++++/:    ',
+      '           `::-            ',
+      '           .ooo+-    ./o`  ',
+      '            oooooo+/+ooo   ',
+      '            /oooooooooo/   ',
+      '            :oooooooooo-   ',
+      '             `-::::::.` ',
       '',
-    ].join('\n')), `\n  ${chalk.whiteBright(`Version v${version}`)}\n`);
+    ].join('\n')), `\n  Looking for nothing but competition.\n`);
+  }
+
+  /**
+   * Desktop notifcation
+   * @param {Object} options Notify options
+   * @returns {NodeNotifier}
+   */
+  static notify(options = {}) {
+    return notifier.notify(options);
   }
 }
 
